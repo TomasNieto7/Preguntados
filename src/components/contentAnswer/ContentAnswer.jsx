103f81afd0983e156a772c2463d0c43280cb6c1c
ELELISEO <jcolunga09@alumnos.uaq.mx>
2024-09-26 13:37:26 -0600

    add: contentAnswer

---
+++ src/components/contentAnswer/ContentAnswer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContentAnswer = () => {
+  return (
+    <>
+        <div className='bg-red-600 h-[5.25rem] w-[25.147rem] rounded-3xl'> 
+        hola soy tomas tonto
+
+        </div>
+    </>
+  )
+}
+
+export default ContentAnswer
--- src/pages/PageInitial.jsx
+++ src/pages/PageInitial.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import ContentAnswer from '../components/contentAnswer/ContentAnswer'
 
 
 const PageInitial = () => {
+  const respuestas = [1, 2]
   return (
     <>
       <main className="h-screen w-screen bg-radial-gradient from-color1 via-color2 to-color2">
         <section className="h-full w-full flex justify-around bg-[url('fondo2.png')]">
-          <div className='ml-10 w-[67rem] h-[21.8rem] bg-white mt-10 rounded-3xl'>
+          <div>
+            <div className='ml-10 w-[67rem] h-[21.8rem] bg-white mt-10 rounded-3xl'>
 
+            </div>
+            <div className='flex justify-around h-96'>
+              <div className='flex justify-around flex-col h-full'>
+                {respuestas.map((index) => <ContentAnswer />)}
+              </div>
+              <div className='flex justify-around flex-col h-full'>
+                {respuestas.map((index) => <ContentAnswer />)}
+              </div>
+            </div>
           </div>
           <div className='w-80 h-[650px] mt-10 bg-[#190620] rounded-3xl border-yellow-500 border-4'>
 
